ed354f462a69ea4b5f9e4cbf8c9b9c7af703c9cf
DESKTOP-UKGFNQ5\wcrwc <wcrwcr1993@gmil.com>
2021-03-29 19:19:21 +0300

    add event onclick mouse(delete square onclick)

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 // тут может находится ваш код
+const squareHeight = 25;
+const squareWidth = 25;
+const maxHeightOfPlatfrom = 480;
+
 
 class Square {
   constructor(x) {
@@ -8,24 +12,26 @@ class Square {
   step(ctx, game) {
     this.clear(ctx);
     this.y = this.y + 1;
-    if(this.y > 510) {
+    if (this.y > maxHeightOfPlatfrom + squareHeight) {
       game.squares.splice(game.squares.indexOf(this, 0), 1);
     } else {
       this.draw(ctx);
-    }    
+    }
   }
   clear(ctx) {
-    ctx.clearRect(this.x, this.y, 25, 25);
+    ctx.clearRect(this.x, this.y, squareWidth, squareHeight);
   }
   draw(ctx) {
     ctx.beginPath();
-    ctx.fillRect(this.x, this.y, 25, 25)
+    ctx.fillRect(this.x, this.y, squareWidth, squareHeight)
     ctx.fillStyle = "black";
     ctx.fill();
     ctx.closePath();
   }
 
 }
+
+
 class Game {
 
   squares = [];
@@ -41,15 +47,12 @@ class Game {
   }
   spawnSquare() {
     let takeRandomWithCanvas = Math.floor(Math.random() * Math.floor(615));
-
-    // определяет можно ли поместить новый квадрат на канвас
     function instersectsWith(existingSquare, newSquare) {
-      if (existingSquare.x > newSquare.x + 50 || existingSquare.x < newSquare.x - 50) {
+      if (existingSquare.x > newSquare.x + 30 || existingSquare.x < newSquare.x - 30) {
         return false;
       }
       return true;
     }
-    // квадрат который мы создали и хотим отрисовать
     let square = new Square(takeRandomWithCanvas);
     let intersects = false;
 
@@ -62,17 +65,32 @@ class Game {
     if (intersects == false) {
       this.squares.push(square);
     }
-
+    // console.log(this.squares);
     setTimeout(this.spawnSquare.bind(this), Math.floor(10 + Math.random() * Math.floor(990)));
   }
-
+  //Получить координаты квадратиков в реальном времени
+  //По клику сверять координаты мышки и квадрата по которому кликнул 
+  //Удалять квадрат из массива
+  fieldClick(e){
+    for (let i = 0; i < this.squares.length; i++) {
+        if (e.clientX >= this.squares[i].x && e.clientX < this.squares[i].x + 26 && e.clientY >= this.squares[i].y && e.clientY < this.squares[i].y + 26){
+          this.squares.splice(this.squares.indexOf(this.squares[i], 0), 1);
+          this.squares[i].clear(this.ctx);
+        }
+    }
+  
+  }
 
   despawnSquare() {
-
+    let callback = function(e) {
+      this.fieldClick(e);
+    };
+    document.querySelector('canvas').onclick = callback.bind(this);
   }
   run() {
     this.spawnSquare();
     this.animate();
+    this.despawnSquare();
   }
 
   start() {
@@ -81,12 +99,46 @@ class Game {
   stop() {
 
   }
+  
+
 }
+
+//
 
 
 let game = new Game();
 
 document.body.onload = game.run();
+
+// function test() {
+//   asd.onclick = function(){
+//     alert('asd');
+//   }
+// }
+
+// class Test1 {
+//   name = 'Shit';
+// }
+
+// class Test2 {
+//   name = 'Fuck';
+// }
+
+// function printName() {
+//   alert(this.name);
+// }
+
+// let obj1 = new Test1();
+// let obj2 = new Test2();
+
+// printName();
+
+// let printTest1Name = printName.bind(obj1);
+// let printTest2Name = printName.bind(obj2);
+
+// printTest1Name();
+// printTest2Name();
+
 
 
 // -25  to 475
